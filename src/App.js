142f4cc2ad6574bd8836fb5860845c39153185df
kucheriavii <kucheriavii@gmail.com>
2023-08-30 19:23:58 +0300

    273 - React Router Error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <Router>
-        <Route path="/" exact={true}>
-          <Dashboard></Dashboard>
-        </Route> 
-        <Route path="/login">
-          <Login></Login>
-        </Route> 
+        <Switch>
+          <Route path="/" exact={true}>
+            <Dashboard></Dashboard>
+          </Route> 
+          <Route path="/login">
+            <Login></Login>
+          </Route> 
+          <Route path="*">
+            <Error></Error>
+          </Route>
+        </Switch>
     </Router>
   );
 }
